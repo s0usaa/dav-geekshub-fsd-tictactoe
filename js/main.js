77b2dbf999ef7ec24aaf92258e1cea60a2ca5e8a
David Soto Sousa <sousavlc86@gmail.com>
2023-02-11 20:55:32 +0100

    He quitado el link del boton press start para cambiar de pantalla con la funcion creada

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let combinacionGanadora = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+];
 
 const comprueboGanador = () => {
     console.log(tableroLogico);
@@ -52,57 +52,52 @@ tablero.map(
     }
 )
 
-//Genero un objeto de JavaScript que va a tener una propiedad para cada player
-
 let players = {
     player1 : "",
     player2 : ""
 }
 
-//Capturo los input por su className... recordad que los captura en una htmlcollection
+//Capturo los input por su className...
 
 let inputs = Array.from(document.getElementsByClassName("inputPlayers"));
 
-//Mapeare el array inputs para darle a todos los elementos el evento addeventlistener input
-//para controlar cuando vayamos escribiendo en ellos
-
+//Mapeo los inputs de los jugadores..
 inputs.map(
     elemento => {
         elemento.addEventListener("input", ()=>{
 
             //Según vamos escribiendo en el input, iremos actualizando el objeto players..
-            //metiendo los valores en la propiedad correspondiente (player1 si escribo en el input de player1 por ejemplo)
 
             for(let jugador in players){
                 if(elemento.name == jugador){
                     //Ahora es cuando meto el valor en el objeto
                     players[jugador] = elemento.value;
+                    
+                    console.log(players)
                 }
             }
         })
     }
 )
 
-//Creo la función cambiaPantalla que guardará en sessionStorage y después cambiará de pantalla
+//Creo la función que guardará en sessionStorage y después cambiará de pantalla
 
 const nuevaPantalla = () => {
 
-    //Primero comprobamos que los nombres de los players NO esten vacios....
+    //Si no tienen valor no continua
 
-    if( (players.player1 === '') || (players.player2 === '') ){
-
-        //Si uno de los 2 está vacio...ejecuto un return y así salgo de la función
+    if( (players.player1 === "") || (players.player2 === "")){
 
         return;
     }
 
-    //Si llego a este punto es porque los nombres si que tenían algún valor..
+    //Si tienen valor entra aqui y guarda los nombres
 
     sessionStorage.setItem("playersInfo", JSON.stringify(players));
 
-    //Después de haber guardado .... cambiamos de página
     setTimeout(()=>{
 
+        //Abre a la ventana del tablero
         window.open("/pages/tablero.html","_self");
-    },1000);
+    },500);
 }
