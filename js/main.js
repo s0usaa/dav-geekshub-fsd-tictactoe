1f6109644d75f4eebf7eea9087f1761f89c7a2fa
David Soto Sousa <sousavlc86@gmail.com>
2023-02-12 19:40:21 +0100

    Botones de reset de partida y jugadores

--- js/main.js
+++ js/main.js
@@ -3,101 +3,100 @@ let fichaP1 = 3;
 let fichaP2 = 3;
 let tablero = Array.from(document.getElementsByClassName("cell"));
 
-let tableroLogico = ["","","","","","","","",""];
+let tableroLogico = ["", "", "", "", "", "", "", "", ""];
 
 let combinacionGanadora = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 const comprueboGanador = () => {
-    console.log(tableroLogico);
+  console.log(tableroLogico);
+};
 
-}
+// Resetear jugadores y partida
+const resetPlayers = () => {
+    
+    sessionStorage.clear();
+    setTimeout(()=>{
+        window.open("/pages/players.html", "_self");
+},500);
+};
 
+//Resetear partida
 
+const resetGame = () => {
+    location.reload();
+};
 
-tablero.map(
-    (casilla) => {
-        casilla.addEventListener('click', ()=> {
+// Pintar el tablero con X y O
+tablero.map((casilla) => {
+  casilla.addEventListener("click", () => {
+    //Compruebo que SI puedo pintar la X o la O.
+    if (casilla.innerHTML === "" && (fichaP1 > 0 || fichaP2 > 0)) {
+      //Aqui PINTO el simbolo, dependiendo del turno
+      casilla.innerHTML = turno ? "X" : "O";
 
+      turno ? fichaP1-- : fichaP2--;
 
-            //Compruebo que SI puedo pintar la X o la O.
-            if((casilla.innerHTML === "") && (fichaP1 > 0 || fichaP2 > 0)){
+      //Finalmente, además de que en pantalla quede marcada, guardo la posición
+      //que ocupa en mi tablero lógico
+      tableroLogico[casilla.id] = turno ? "X" : "O";
 
-                //Aqui PINTO el simbolo, dependiendo del turno
-                casilla.innerHTML = (turno) ? "X" : "O";
+      comprueboGanador();
 
-                //Después, dependiendo tambien de quien fuese el turno, le quito una ficha
-                //de las que puede poner
-                (turno) ? fichaP1-- : fichaP2--;
-
-
-                //Finalmente, además de que en pantalla quede marcada, guardo la posición 
-                //que ocupa en mi tablero lógico
-                tableroLogico[casilla.id] = (turno) ? "X" : "O";
-
-                comprueboGanador();
-
-                //Cambiamos de turno
-                turno = !turno;
-            }
-        })
+      //Cambiamos de turno
+      turno = !turno;
     }
-)
+  });
+});
 
 let players = {
-    player1 : "",
-    player2 : ""
-}
+  player1: "",
+  player2: "",
+};
 
 //Capturo los input por su className...
 
 let inputs = Array.from(document.getElementsByClassName("inputPlayers"));
 
 //Mapeo los inputs de los jugadores..
-inputs.map(
-    elemento => {
-        elemento.addEventListener("input", ()=>{
+inputs.map((elemento) => {
+  elemento.addEventListener("input", () => {
+    //Según vamos escribiendo en el input, iremos actualizando el objeto players..
 
-            //Según vamos escribiendo en el input, iremos actualizando el objeto players..
+    for (let jugador in players) {
+      if (elemento.name == jugador) {
+        //Ahora es cuando meto el valor en el objeto
+        players[jugador] = elemento.value;
 
-            for(let jugador in players){
-                if(elemento.name == jugador){
-                    //Ahora es cuando meto el valor en el objeto
-                    players[jugador] = elemento.value;
-                    
-                    console.log(players)
-                }
-            }
-        })
+        console.log(players);
+      }
     }
-)
+  });
+});
 
 //Creo la función que guardará en sessionStorage y después cambiará de pantalla
 
 const nuevaPantalla = () => {
+  //Si no tienen valor no continua
 
-    //Si no tienen valor no continua
+  if (players.player1 === "" || players.player2 === "") {
+    return;
+  }
 
-    if( (players.player1 === "") || (players.player2 === "")){
+  //Si tienen valor entra aqui y guarda los nombres
 
-        return;
-    }
+  sessionStorage.setItem("playersInfo", JSON.stringify(players));
 
-    //Si tienen valor entra aqui y guarda los nombres
-
-    sessionStorage.setItem("playersInfo", JSON.stringify(players));
-
-    setTimeout(()=>{
-
-        //Abre a la ventana del tablero
-        window.open("/pages/tablero.html","_self");
-    },500);
-}
+  setTimeout(() => {
+    //Abre a la ventana del tablero
+    window.open("/pages/tablero.html", "_self");
+  }, 500);
+};
