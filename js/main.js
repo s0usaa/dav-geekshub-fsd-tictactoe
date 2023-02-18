ea04190c2ddba14c07c3770436a916ff7da30dcf
David Soto Sousa <sousavlc86@gmail.com>
2023-02-18 23:08:48 +0100

    Pagina del tablero terminada

--- js/main.js
+++ js/main.js
@@ -21,35 +21,35 @@ let combinacionGanadora = [
 const comprueboGanador = () => {
         //Combi 1
         if ((tableroLogico[0] === tableroLogico[1]) && (tableroLogico[1] === tableroLogico[2]) && tableroLogico[0] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 2
         }else if ((tableroLogico[3] === tableroLogico[4]) && (tableroLogico[3] === tableroLogico[5]) && tableroLogico[3] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 3
         }else if ((tableroLogico[6] === tableroLogico[7]) && (tableroLogico[6] === tableroLogico[8] && tableroLogico[6] !== "")){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 4
         }else if ((tableroLogico[0] === tableroLogico[3]) && (tableroLogico[0] === tableroLogico[6]) && tableroLogico[0] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 5
         }else if ((tableroLogico[1] === tableroLogico[4]) && (tableroLogico[1] === tableroLogico[7]) && tableroLogico[1] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 6
         }else if ((tableroLogico[2] === tableroLogico[5]) && (tableroLogico[2] === tableroLogico[8]) && tableroLogico[2] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 7
         }else if ((tableroLogico[0] === tableroLogico[4]) && (tableroLogico[0] === tableroLogico[8]) && tableroLogico[0] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         //Combi 8
         }else if((tableroLogico[2] === tableroLogico[4]) && (tableroLogico[2] === tableroLogico[6]) && tableroLogico[2] !== ""){
-          turno ? sessionStorage.setItem("ganador", "X"): sessionStorage.setItem("ganador", "O");
+          turno ? sessionStorage.setItem("ganador", "x"): sessionStorage.setItem("ganador", "o");
           window.open("../pages/winners.html","_self");
         }
 }
@@ -62,7 +62,7 @@ const resetPlayers = () => {
 },500);
 };
 
-//Resetear partida
+// Resetear partida
 const resetGame = () => {
     window.location.reload();
 };
@@ -70,29 +70,28 @@ const resetGame = () => {
 // Pintar el tablero
 tablero.map((casilla) => {
   casilla.addEventListener("click", () => {
-    //Compruebo que SI puedo pintar la X o la O.
     if (casilla.innerHTML === "" && (fichaP1 > 0 || fichaP2 > 0)) {
       //Aqui PINTO el simbolo, dependiendo del turno
-      casilla.innerHTML = turno ? "X" : "O";
+      casilla.innerHTML = turno ? "x" : "o";
       //Restamos una ficha al jugador
       turno ? fichaP1-- : fichaP2--;
-      document.getElementById("player1Text").innerHTML = `Te quedan ${fichaP1} fichas`;
-      document.getElementById("player2Text").innerHTML = `Te quedan ${fichaP2} fichas`;
+      document.getElementById("player1Text").innerHTML = `Fichas restantes ${fichaP1}`;
+      document.getElementById("player2Text").innerHTML = `Fichas restantes ${fichaP2}`;
     //   guardo la posición que ocupa en mi tablero lógico
-      tableroLogico[casilla.id] = turno ? "X" : "O";
+      tableroLogico[casilla.id] = turno ? "x" : "o";
       comprueboGanador();
       //Cambiamos de turno
       turno = !turno;
-    }else if (casilla.innerHTML === "X" && fichaP1 === 0 && turno == true){
+    }else if (casilla.innerHTML === "x" && fichaP1 === 0 && turno == true){
       casilla.innerHTML = "";
       tableroLogico[casilla.id] = "";
       fichaP1++;
-      document.getElementById("player1Text").innerHTML = `Te quedan ${fichaP1} fichas`;
-    }else if (casilla.innerHTML === "O" && fichaP2 === 0 && turno == false){
+      document.getElementById("player1Text").innerHTML = `Fichas restantes ${fichaP1}`;
+    }else if (casilla.innerHTML === "o" && fichaP2 === 0 && turno == false){
       casilla.innerHTML = "";
       tableroLogico[casilla.id] = "";
       fichaP2++;
-      document.getElementById("player2Text").innerHTML = `Te quedan ${fichaP2} fichas`;
+      document.getElementById("player2Text").innerHTML = `Fichas restantes ${fichaP2}`;
 }})
 })
 
@@ -106,7 +105,6 @@ let inputs = Array.from(document.getElementsByClassName("inputPlayers"));
 //Mapeo los inputs de los jugadores..
 inputs.map((elemento) => {
   elemento.addEventListener("input", () => {
-    //Según vamos escribiendo en el input, iremos actualizando el objeto players..
     for (let jugador in players) {
       if (elemento.name == jugador) {
         //Ahora es cuando meto el valor en el objeto
@@ -118,14 +116,11 @@ inputs.map((elemento) => {
 
 //Creo la función que guardará en sessionStorage y después cambiará de pantalla
 const nuevaPantalla = () => {
-  //Si no tienen valor no continua
-
   if (players.player1 === "" || players.player2 === "") {
     return;
   }
   sessionStorage.setItem("playersInfo", JSON.stringify(players));
   setTimeout(() => {
-    //Abre a la ventana del tablero
     window.open("../pages/tablero.html", "_self");
   }, 500);
 };
